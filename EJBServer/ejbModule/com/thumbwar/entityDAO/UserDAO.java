bc8cf433e627d7562d1e472faabb809ef0fbf243
sinnott74 <sinnott74@hotmail.com>
2014-10-27 23:33:41 +0000

    Changed hardcoded SQL into JPA equivalent

--- EJBServer/ejbModule/com/thumbwar/entityDAO/UserDAO.java
+++ EJBServer/ejbModule/com/thumbwar/entityDAO/UserDAO.java
@@ -1,6 +1,13 @@
 package com.thumbwar.entityDAO;
 
+import java.lang.reflect.ParameterizedType;
+
 import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.ParameterExpression;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import com.thumbwar.entity.User;
 
@@ -14,10 +21,16 @@ public class UserDAO extends DaoImpl<User> {
 	
 
 	public User findByUsername(String username){
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<User> q = cb.createQuery(User.class);
+		Root<User> user = q.from(User.class);
+		Predicate condition1 = cb.equal(user.get("username"), username);
+		q.where(condition1);
+		q.select(user);
+		return em.createQuery(q).getSingleResult();
 		
-		
-		Query q = em.createQuery("SELECT u FROM User u WHERE u.username =:username");
-		q.setParameter("username", username);
-		return (User) q.getResultList().get(0);
+//		Query q = em.createQuery("SELECT u FROM User u WHERE u.username =:username");
+//		q.setParameter("username", username);
+//		return (User) q.getResultList().get(0);
 	}
 }
