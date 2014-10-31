1a6724a81a34137ffd15b9f81ac20546117d5ee3
Sinnott74 <sinnott74@hotmail.com>
2014-10-31 14:36:31 +0000

    removed comment

--- WebClient/src/dansin/test/struts/TestAction.java
+++ WebClient/src/dansin/test/struts/TestAction.java
@@ -9,11 +9,11 @@ import javax.naming.NamingException;
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.Preparable;
 
+import dansin.test.ejb.TestBean;
 import dansin.test.ejb.TestBeanRemote;
 
 public class TestAction extends ActionSupport implements Preparable {
 
-	//@com.google.inject.Inject
 	TestBeanRemote testBeanRemote;
 
 	public String TestGuice() {
@@ -29,7 +29,7 @@ public class TestAction extends ActionSupport implements Preparable {
 		prop.put(Context.INITIAL_CONTEXT_FACTORY, "org.apache.openejb.client.RemoteInitialContextFactory");
 		prop.put("java.naming.provider.url", "ejbd://localhost:4201");
 		Context context = new InitialContext(prop);
-		testBeanRemote =(TestBeanRemote)context.lookup("dansin.test.ejb.TestBean");
+		testBeanRemote =(TestBeanRemote)context.lookup(TestBean.class.getName());
 	}
 
 	@Override
